910da78bd0c4afce277104a4a52f5f00cb9c4e23
softlolx <softlolx@gmail.com>
2022-06-23 23:12:44 +0300

    feat: swipe up down and score counting

--- scripts/index.js
+++ scripts/index.js
@@ -2,16 +2,19 @@ const startButton = document.querySelector(".start-button");
 const rulesBlock = document.querySelector(".rules");
 const boardBlock = document.querySelector(".board");
 const gamefield = document.querySelector(".gamefield");
+const actualScore = document.querySelector("#actual-score");
 
 function openGame() {
   rulesBlock.classList.add("rules_hidden");
   boardBlock.classList.remove("board_hidden");
 }
 
+let actScore = 0;
+
 let board = [
-  [2, 2, 2, 2],
-  [16, 4, 4, 8],
-  [0, 2, 2, 0],
+  [0, 0, 0, 2],
+  [0, 0, 0, 0],
+  [0, 0, 0, 0],
   [0, 0, 0, 0],
 ];
 
@@ -19,8 +22,25 @@ function clearBoard() {
   gamefield.innerHTML = "";
 }
 
+function addScore(score) {
+  actScore += score;
+
+  actualScore.innerHTML = actScore;
+}
+
+function generateRandomCell() {
+  let row = Math.floor(Math.random() * 4);
+  let col = Math.floor(Math.random() * 4);
+  let num = Math.floor(Math.random() * 2 + 1) * 2;
+  if (board[row][col] == 0) {
+    board[row][col] = num;
+  }
+}
+
 function generateBoard() {
   clearBoard();
+  generateRandomCell();
+
   board.forEach((row, rowNum) => {
     row.forEach((column, colNum) => {
       let cell = document.createElement("div");
@@ -45,6 +65,7 @@ function swipe(row) {
     if (noZeroesRow[i] == noZeroesRow[i + 1]) {
       noZeroesRow[i] *= 2;
       noZeroesRow[i + 1] = 0;
+      addScore(+noZeroesRow[i]);
     }
   }
 
@@ -77,6 +98,50 @@ function moveCellsRight() {
   generateBoard();
 }
 
+function moveCellsUp() {
+  board.forEach((row, rowNum) => {
+    let horizontalRow = [
+      board[0][rowNum],
+      board[1][rowNum],
+      board[2][rowNum],
+      board[3][rowNum],
+    ];
+
+    let newRow = swipe(horizontalRow);
+
+    board[0][rowNum] = newRow[0];
+    board[1][rowNum] = newRow[1];
+    board[2][rowNum] = newRow[2];
+    board[3][rowNum] = newRow[3];
+  });
+
+  generateBoard();
+}
+
+function moveCellsDown() {
+  board.forEach((row, rowNum) => {
+    let horizontalRow = [
+      board[0][rowNum],
+      board[1][rowNum],
+      board[2][rowNum],
+      board[3][rowNum],
+    ];
+
+    horizontalRow.reverse();
+
+    let newRow = swipe(horizontalRow);
+
+    newRow.reverse();
+
+    board[0][rowNum] = newRow[0];
+    board[1][rowNum] = newRow[1];
+    board[2][rowNum] = newRow[2];
+    board[3][rowNum] = newRow[3];
+  });
+
+  generateBoard();
+}
+
 startButton.addEventListener("click", () => {
   openGame();
   generateBoard();
@@ -87,9 +152,9 @@ document.addEventListener("keyup", (evt) => {
     moveCellsLeft();
   } else if (evt.key == "ArrowRight") {
     moveCellsRight();
+  } else if (evt.key == "ArrowUp") {
+    moveCellsUp();
+  } else if (evt.key == "ArrowDown") {
+    moveCellsDown();
   }
 });
-//temp
-
-openGame();
-generateBoard();
