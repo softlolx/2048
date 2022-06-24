38aaaf6d846fcfdea7065e9a889ba2bb228a65b8
softlolx <softlolx@gmail.com>
2022-06-24 15:34:42 +0300

    feat: touch support testing

--- scripts/swipe.js
+++ scripts/swipe.js
@@ -1,44 +1,87 @@
+// let gesuredZone = document.querySelector("#board");
+
+// gesuredZone.addEventListener(
+//   "touchstart",
+//   function (event) {
+//     touchstartX = event.screenX;
+//     touchstartY = event.screenY;
+//   },
+//   false
+// );
+
+// gesuredZone.addEventListener(
+//   "touchend",
+//   function (event) {
+//     touchendX = event.screenX;
+//     touchendY = event.screenY;
+//     handleGesure();
+//   },
+//   false
+// );
+
+// function handleGesure() {
+//   let swiped = "swiped: ";
+//   if (touchendX < touchstartX) {
+//     moveCellsLeft();
+//   }
+//   if (touchendX > touchstartX) {
+//     moveCellsRight();
+//   }
+//   if (touchendY < touchstartY) {
+//     moveCellsDown();
+//   }
+//   if (touchendY > touchstartY) {
+//     moveCellsUp();
+//   }
+//   if (touchendY == touchstartY) {
+//     console.log("touch");
+//   }
+// }
+
 let touchstartX = 0;
 let touchstartY = 0;
 let touchendX = 0;
 let touchendY = 0;
 
-let gesuredZone = document.querySelector("#board");
+const gestureZone = document.querySelector("#board");
 
-gesuredZone.addEventListener(
+gestureZone.addEventListener(
   "touchstart",
   function (event) {
-    touchstartX = event.screenX;
-    touchstartY = event.screenY;
+    touchstartX = event.changedTouches[0].screenX;
+    touchstartY = event.changedTouches[0].screenY;
   },
   false
 );
 
-gesuredZone.addEventListener(
+gestureZone.addEventListener(
   "touchend",
   function (event) {
-    touchendX = event.screenX;
-    touchendY = event.screenY;
-    handleGesure();
+    touchendX = event.changedTouches[0].screenX;
+    touchendY = event.changedTouches[0].screenY;
+    handleGesture();
   },
   false
 );
 
-function handleGesure() {
-  let swiped = "swiped: ";
+function handleGesture() {
   if (touchendX < touchstartX) {
     moveCellsLeft();
   }
+
   if (touchendX > touchstartX) {
     moveCellsRight();
   }
+
   if (touchendY < touchstartY) {
-    moveCellsDown();
-  }
-  if (touchendY > touchstartY) {
     moveCellsUp();
   }
-  if (touchendY == touchstartY) {
-    console.log("touch");
+
+  if (touchendY > touchstartY) {
+    moveCellsDown();
+  }
+
+  if (touchendY === touchstartY) {
+    console.log("Tap");
   }
 }
